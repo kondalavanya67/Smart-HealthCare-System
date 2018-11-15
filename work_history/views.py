c5a0721877fcb70df465405dd3a7857317cfa12d
Shivam Gupta <shivamguptahdr98@gmail.com>
2018-11-16 00:35:14 +0530

    Worked on  Work_History app

--- work_history/views.py
+++ work_history/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
+from prescription.models import Prescription
+from doctor_profile.models import Profile
 
 def index(request):
-    return render(request,'work_history_home.html')
+    user=request.user
+    profile = Profile.objects.get(user=user)
+    prescriptions=Prescription.objects.filter(doctor=profile)
+    print(user)
+    print("**")
+    print(prescriptions[0].prescription_date)
+
+    return render(request,'work_history_home.html',{'prescriptions':prescriptions})
