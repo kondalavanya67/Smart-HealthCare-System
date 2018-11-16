147430996460147cc8f4ead5fed239eeaaff72fa
Shivam Gupta <shivamguptahdr98@gmail.com>
2018-11-16 19:14:23 +0530

    Basic Work_history App completed

--- work_history/views.py
+++ work_history/views.py
@@ -9,8 +9,11 @@ def index(request):
     user=request.user
     profile = Profile.objects.get(user=user)
     prescriptions=Prescription.objects.filter(doctor=profile)
+    first_name=profile.first_name
+    last_name=profile.last_name
     print(user)
     print("**")
+    print(prescriptions[0].pdf)
     print(prescriptions[0].prescription_date)
 
-    return render(request,'work_history_home.html',{'prescriptions':prescriptions})
+    return render(request,'work_history_home.html',{'prescriptions':prescriptions,'first_name':first_name,'last_name':last_name})
