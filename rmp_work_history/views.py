bda6280ee7948d7b38ee7f2128910c58d0d1037d
Kalpajyoti <kalpahe@gmail.com>
2018-12-06 19:52:50 +0530

    kalp

--- rmp_work_history/views.py
+++ rmp_work_history/views.py
@@ -12,6 +12,17 @@ def index(request):
     context={
         "appointments":appointments,
     }
-    print(appointments)
+  
     
     return render(request,'rmp_work_history/index.html',context=context)
+
+def history(request):
+    user=request.user
+    appointments1 = AppointmentDetials.objects.filter(user=user).filter(is_attended=True)
+    context={
+        "appointments1":appointments1,
+    }
+    
+    return render(request,'rmp_work_history/history.html',context=context)
+
+
