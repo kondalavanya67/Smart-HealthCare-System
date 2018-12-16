9609d4cd5fc7cea17f3a860c50748e339d2e70dd
Shivam Gupta <shivamguptahdr98@gmail.com>
2018-12-16 12:06:57 +0530

    Made Changes in Frontend and views testing

--- prescription/urls.py
+++ prescription/urls.py
@@ -5,6 +5,7 @@ app_name='prescription'
 urlpatterns=[
     path('',views.index,name='index'),
     url(r'^prescription_add/(?P<appointment_id>[0-9]+)$',views.PrescriptionCreate.as_view(),name='Prescription_Add1'),
+    path('prescription_add/',views.make_prescription,name='Prescription_Add'),
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
     url(r'^(?P<prescription_id>[0-9]+)/create_item/$', views.create_item, name='create_item'),
     path('name-autocomplete/',views.MedicineAutocomplete.as_view(),name='name-autocomplete'),
